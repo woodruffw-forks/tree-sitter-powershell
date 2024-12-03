c7d3781f54993aeb7f365acccd3c8bfb393953f7
William Woodruff <william@yossarian.net>
2024-12-03 00:57:23 -0500

    simplify test

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -4,9 +4,7 @@
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = r#"
-//! foo
-//! "#;
+//! let code = r"foo";
 //! let mut parser = tree_sitter::Parser::new();
 //! let language = tree_sitter_powershell::LANGUAGE;
 //! parser
